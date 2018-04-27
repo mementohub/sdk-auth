92786d824f52364e04ee92af63f58ed3e281bd18
George Stan <contact@georgestan.com>
2018-04-27 13:43:05 +0300

    service authenticate

--- src/Helper.php
+++ src/Helper.php
@@ -8,6 +8,7 @@ use Crypt;
 use Session;
 use Redirect;
 use Request;
+use GuzzleHttp\Client;
 use iMemento\JWT\JWT;
 
 /**
@@ -24,6 +25,8 @@ class Helper
     protected static $app_name = 'APP_NAME';
 
     /**
+     * Redirects to auth login or register
+     *
      * @param string $callback_url
      * @param string $return_url
      * @param bool   $register
@@ -46,6 +49,8 @@ class Helper
     }
 
     /**
+     * Handles the redirect from auth and authenticates the user based on the returned token
+     *
      * @param string $auth_jwt
      * @param        $auth_public_key
      * @param        $user
@@ -76,6 +81,34 @@ class Helper
         Session::put('user', Crypt::encryptString($user->toJson())); //used in the static user provider
 
         Auth::login($user);
+    }
+
+    /**
+     * Authenticates a user (or service) based on email and password, and returns the jwt
+     *
+     * @param string $email
+     * @param string $password
+     * @return mixed
+     */
+    public static function authenticate(string $email, string $password)
+    {
+        $client = new Client;
+
+        $scheme = Request::secure() ? 'https://' : 'http://';
+        $url = $scheme . env(self::$url_key) . 'api/v1/authenticate';
+
+        $data = [
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+            'json' => [
+                'email' => $email ?? env('APP_EMAIL'),
+                'password' => $password ?? env('APP_PASSWORD'),
+            ],
+        ];
+
+        $response = $client->post($url, $data);
+        return json_decode($response->getBody()->getContents());
     }
 
 }
